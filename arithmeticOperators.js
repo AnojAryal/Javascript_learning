335f03d6d7ba126d1a8ec55ce9662d8e69b39af3
AnojAryal <anoj1810@gmail.com>
2023-04-06 19:54:00 +0545

    arithmetic operators in js

---
+++ arithmeticOperators.js
@@ -0,0 +1,56 @@
+let num1 = 10
+let num2 = 15
+
+let addition = num1 + num2
+console.log(addition);
+
+let subtraction = num2 - num1
+console.log(subtraction);
+
+let multiplication = num1 * num2
+console.log(multiplication);
+
+let division = 10 /5 
+console.log(division);
+
+let remainder = 10 % 2
+console.log(remainder);
+
+let num3 = true 
+let num4 = true 
+console.log(num3 + num4);
+
+num1 = num1 + 20 
+console.log(num1);
+
+num1+= 30
+console.log(num1);
+
+num1++   //post increment 
+console.log(num1);
+
+num1--
+console.log(num1);
+
+++num1  //pre increment 
+console.log(num1);
+
+let sum = 4
+let e = sum++
+console.log(sum , e);   //post increment
+
+sum = 4
+e = ++sum
+console.log(sum , e);   //pre increment
+
+let n = 4
+let power = Math.pow(4,3)
+console.log(power);
+
+t = 4**3
+console.log(t); 
+
+
+
+
+

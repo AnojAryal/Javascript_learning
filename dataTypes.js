d1a23d0065ace0d387f0c92795b6841c91bef9a3
AnojAryal <anoj1810@gmail.com>
2023-04-06 18:09:11 +0545

    Datatypes in javascript

---
+++ dataTypes.js
@@ -0,0 +1,93 @@
+//primitive and object --datatypes 
+
+
+//Number
+
+let data = 8  //number
+let userName = "FraNzY" //string
+
+
+let num1 = 123456
+console.log(num1*2);
+
+let num2 = 7.5
+console.log(typeof num2);
+
+console.log(typeof userName);
+
+let num3 = 0xE
+console.log(num3);
+
+let num4 = 9.5e12
+console.log(num4);
+
+let num5 = 100_00_000
+console.log(num5);
+
+let num6 = 7/0
+console.log(num6);   
+//infinity is also a type of number in js 
+
+
+//if we divide a negative number by 0 it will give negative infinity
+let num7 = -8/0
+console.log(num7);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE * 10);
+//whenever we go beyod maximum value we get  infinity 
+
+console.log(1910103874833930303903033);
+console.log(1910103874833930303903033n);
+
+
+//String
+
+let Name = "FraNzY" 
+
+console.log(userName);
+
+let firstName = "Anoj"
+let lastName = "Aryal"
+
+console.log(firstName +" "+ lastName);
+
+let Abc = "Anoj Aryal \n \"FraNzY\" "
+console.log(Abc);
+
+
+//boolean 
+ 
+let bool = 5>6
+console.log(bool);
+console.log(typeof  bool);
+
+
+//Null
+
+//Null represents an object but it says it isnt an object
+// it doesnt represent an object but it is an object
+let N = null
+console.log(N);
+console.log(typeof N); //which is a bug(error)
+
+let M
+console.log(M);
+console.log(typeof M);
+
+
+let o = 5
+console.log(o/"Anoj");
+console.log(5/"Anoj");
+console.log( typeof (5/"Anoj"));
+console.log( typeof (o/"Anoj"));
+
+
+
+
+
+
+
+
+
+

fa6c40ce6f47d68538cea01bcc2c1b8e9bd37ef3
AnojAryal <anoj1810@gmail.com>
2023-04-08 11:02:12 +0545

    relational operators

---
+++ relationalOperators.js
@@ -0,0 +1,37 @@
+console.log(5>6);
+console.log(7>6);
+
+let a = 5
+let b = 7
+
+data = a<= b
+
+console.log(data);
+
+let  x = "pen"
+let y = "book"
+
+data1 = x > y  //in case of string it works as alphabetical wise 
+
+console.log(data1);
+
+
+let k =  "pen"
+let l = "pencil"
+
+data2 = k > l   //in this case pencil has more characters tha pen so it is >
+
+console.log(data2);
+
+
+let M = "5"
+let N = 5
+
+
+// in case of == it will only check the data
+// but in case of === it will chck the type and data both 
+
+data3 = M == N  //in case of string and integer --string is also stored and it has some character
+data4 = M === N
+console.log(data3);
+console.log(data4);

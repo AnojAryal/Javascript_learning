0fddc3be53acd2ad6915e9dd385ccc1b666e1287
AnojAryal <anoj1810@gmail.com>
2023-04-12 12:16:08 +0545

    while and do while in javascript

---
+++ whileLoop.js
@@ -0,0 +1,33 @@
+console.log("Hi");
+
+//to print same statement for 110 times
+
+let i = 1;
+
+while(i<=10) 
+{
+    console.log("Hi"+ "-----" + i);
+    i++
+}
+
+console.log("--------------------------------------")
+
+let j = 10;
+
+while(j>=0) 
+{
+    console.log("Hi"+ "-----" + j);
+    j--
+}
+
+console.log("--------------------------------------")
+
+//do while 
+
+let k = 1;
+
+do
+{
+    console.log("Hi"+ "-----" + k);
+    k++
+}while(k<=10)

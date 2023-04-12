62fe2b3dcd59fde63820f5a4e5f19cf3fcbb5b2d
AnojAryal <anoj1810@gmail.com>
2023-04-12 11:49:11 +0545

    template literal

---
+++ templateLiteral.js
@@ -0,0 +1,15 @@
+let num1= 13;
+let num2 = 7;
+
+let result = num1 + num2;
+
+// console.log("The output of" + num1 + "and" + num2 + "is" + result);
+
+//we can use template literal instaed of it 
+
+console.log(`The output of ${num1} and ${num2} is ${result} `);
+
+console.log("My Name is \nFraNzY");
+
+console.log(`My Name is
+FraNzY`);

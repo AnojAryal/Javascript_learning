22bd6ed49b60293b4e45468985b087373a18a9ea
AnojAryal <anoj1810@gmail.com>
2023-04-17 20:03:48 +0545

    method in js

---
+++ Methods.js
@@ -0,0 +1,16 @@
+//adding function to an object 
+
+//object will have data and behaviour both 
+
+let laptop = {
+    cpu : "i7",
+    ram : 8,
+    brand : "dell",
+
+
+    greet: function(){
+        console.log("hello world");
+    }
+}
+
+laptop.greet()

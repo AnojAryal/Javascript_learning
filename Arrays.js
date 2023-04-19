41f508b6e2ac4a2b6f2b2f41288008e3ffc92592
AnojAryal <anoj1810@gmail.com>
2023-04-19 23:53:04 +0545

    array in js

---
+++ Arrays.js
@@ -0,0 +1,18 @@
+let values = [5,7,8,9];
+
+//pushing the values by using push 
+values.push(6,3);
+
+let Names =['Anoj','Baibhav','krishal'];
+
+//pushing the value in array by mentioning the indent 
+Names[3]='Pravin';
+
+//array can contain  string,integer,objects and function too
+let data =['Ram',18,{tech :'JS'},function(){console.log("hello world")}]
+
+console.log(Names);
+console.log(values[4]);
+console.log(data);
+
+data[3]();

269013774a3763d495f30f67b28828d9c8f8d3a2
AnojAryal <anoj1810@gmail.com>
2023-04-20 00:07:51 +0545

    array methods in javascript

---
+++ arrayMethods.js
@@ -0,0 +1,19 @@
+let data =[5,7,8,9,4,3]
+
+//it will add 2 at lst 
+console.log(data.push(2));
+
+//it will remove the last element
+console.log(data.pop());
+
+//it will throw first element out of array <----
+console.log(data.shift());
+
+//it will add an element at beginning of array
+console.log(data.unshift(5));
+
+//it will remove only one number from second element in array
+console.log(data.splice(2,1));
+
+
+console.log(data);
